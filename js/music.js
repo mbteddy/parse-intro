bdb1d1f5ee6cf65131f6e8975234c6b452cea2bf
Teddy Pappas <mbteddy@verizon.net>
2015-10-20 10:20:25 -0700

    first commit

--- js/music.js
+++ js/music.js
@@ -1,35 +1,44 @@
 // Initialize Parse app
-
+Parse.initialize("W9uSW9Sdf2kFfYf8NdbIfcxGLTbwdn4IErQM7Gt2", "QTQVTBGt1SVPCrSlCG3oX2knwjYJoHUNPPWz7OaZ")
 
 // Create a new sub-class of the Parse.Object, with name "Music"
-
+var Music = Parse.Object.extend("Music");
 
 // Create a new instance of your Music class 
-
+var sweetTune = new Music();
 
 // Set a property 'band' equal to a band name
-
+sweetTune.set('band','Muse');
 
 // Set a property 'website' equal to the band's website
-
+sweetTune.set('website','Muse.com');
     
 // Set a property 'song' equal to a song
-
+sweetTune.set('song','best song');
 
 // Save your instance of your song -- and go see it on parse.com!
-
+sweetTune.save();
 
 // Click event when form is submitted
 $('form').submit(function() {
 
 	// Create a new instance of your Music class 
-
+	var newTune = new Music();
 
 	// For each input element, set a property of your new instance equal to the input's value
-
-
+	var band = $('#band').val()
+	newTune.set('band', band)
+	var website = $('#website').val()
+	newTune.set('website', website)
+	var song = $('#song').val()
+	newTune.set('song', song)
 	// After setting each property, save your new instance back to your database
+	newTune.save()
 
+	//clear it out
+	$('#band').val(' ')
+	$('#website').val(' ')
+	$('#song').val(' '))
 	
 	return false
 })
